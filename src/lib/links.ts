fa33ee9e390dc04f21237c0d0ff1ed9eb2911143
Keegan Shirley <shirlekw@dukes.jmu.edu>
2026-08-06 13:42:49 -0400

    Rename nav link from "Join List" to "Join Chat"

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -30,7 +30,7 @@ export const NAV_LINKS = [
   { label: "About", href: "/#about" },
   { label: "Merch", href: "/merch" },
   { label: "Connect", href: "/#connect" },
-  { label: "Join List", href: "/join" },
+  { label: "Join Chat", href: "/join" },
 ] as const;
 
 export const MUSIC_VIDEO_URL = "https://www.youtube.com/watch?v=V1JrGTPAUuU";
